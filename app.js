09b34178f16fa45c7ed5315551f997629c3ff1ee
marco amaral <amaral.marco@gmail.com>
2016-02-17 16:07:08 +0000

    work

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var api_vlc = require('./routes/api-vlc');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/api-vlc', api_vlc);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/api-vlc.js
+++ routes/api-vlc.js
@@ -1,11 +1,73 @@
 var express = require('express');
 var router = express.Router();
-var http = require ('http');
+var r = require("request");
+var parseString = require('xml2js').parseString;
 
-router.all('/api-vlc/status', function(req, res, next){
-    http.get('http://127.0.0.1/status.xml&' );
+var pass = 'password';
+var authHeader = 'Basic ' +  (new Buffer(':' + pass, 'utf8')).toString('base64') ;
+
+var request = r.defaults({
+    headers:{
+        'Authorization':authHeader
+    }
 });
 
-router.all('/api-vlc/play', function(req, res, next){
-    http.get('http://127.0.0.1/');
-})
+router.all('/playlist', function(req, res, next){
+    request.get('http://127.0.0.1:8080/playlist.xml' );
+});
+
+router.all('/status', function(req, res, next){
+    request.get('http://127.0.0.1:8080/status.xml');
+});
+
+router.all('/add', function (req, res, next) {
+    var m = req.query.music ? encodeURIComponent(req.query.music): null;
+    if(!m){
+        res.send('erro: nenhuma musica');
+        return;
+    }
+    request.get('http://127.0.0.1:8080/requests/status.xml',
+        function (error, response, body) {
+            if(error){
+                console.log(error);
+                res.send(error);
+                return;
+            }
+
+            if (!error && response.statusCode == 200) {
+                parseString(body, function (err, result) {
+                    if (result.root.currentplid[0] >-1) {
+                        request.get('http://127.0.0.1:8080/requests/status.xml?command=in_enqueue&input=' + m,
+                            function (error, response, body) {
+                                res.status(200).send('');
+                            }
+                        );
+                    } else {
+                        request.get('http://127.0.0.1:8080/requests/status.xml?command=in_play&input=' + m,
+                            function (error, response, body) {
+                                res.status(200).send('');
+                            }
+                        );
+                    }
+                });
+            } else {
+                res.status(500).send(error);
+            }
+        }
+    );
+});
+
+router.all('/play', function(req, res, next){
+    request.get('http://127.0.0.1:8080/requests/status.xml?command=pl_pause');
+});
+
+router.all('/pause', function(req, res, next){
+    request.get('http://127.0.0.1:8080/requests/status.xml?command=pl_pause');
+});
+
+router.all('/stop', function(req, res, next){
+    request.get('http://127.0.0.1:8080/requests/status.xml?command=pl_stop');
+});
+
+
+module.exports = router;
